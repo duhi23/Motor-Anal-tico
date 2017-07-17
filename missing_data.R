0d3e42835f8069a55ac46a63fcb241c8135050b6
duhi23 <diego.21.phs@hotmail.com>
2017-07-17 15:13:13 -0500

    Final Update

--- missing_data.R
+++ missing_data.R
@@ -85,15 +85,35 @@ tabla <- data.frame(variable=c("SALDO_NOR_M1", "SALDO_NOR_M5", "SALDO_NOR_M10", 
 # data: datos en formato data.table sin valores perdidos
 # criterio: tabla con las acciones a tomar sobre las variables
 # vars: vector de variables con valores atipicos
-# 
+# retorna los datos eliminados o corregidos en base al criterio ingresado
 atipico <- function(data, criterio, vars){
       sub_cri <- criterio[criterio[,2]=="eliminar",]
       nvars <- nrow(sub_cri)
       if(nvars>0){
-            cols_dlt <- c()
-            for(i in 1:nvars){
-                  cols_dlt <- c(grep(sub_cri[[1]][i], vars)[1], cols_dlt)
+            ind_atp <- function(data){
+                  numcol <- ncol(data)
+                  vec <- c()
+                  for(i in 1:numcol){
+                        valsup <- median(data[[i]]) + 1.5*IQR(data[[i]])
+                        valinf <- median(data[[i]]) - 1.5*IQR(data[[i]])
+                        ind <- which(data[[i]] > valsup | data[[i]] < valinf)
+                        vec <- c(vec, ind)
+                  }
+                  return(unique(vec))
             }
+            data <- data[-ind_atp(data[,sub_cri[[1]], with=FALSE]),]
       }
+      atp <- function(x){
+            valsup <- median(x) + 1.5*IQR(x)
+            valinf <- median(x) - 1.5*IQR(x)
+            x[x > median(x) + 1.5*IQR(x)] <- valsup
+            x[x < median(x) - 1.5*IQR(x)] <- valinf
+            return(x)
+      }
+            lapply(1:length(vars), function(i){cols <- vars[i]; 
+            data[ , (cols) := lapply(.SD, atp), .SDcols = cols]})
+      return(data)
 }
+
+
 
