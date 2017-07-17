5f29aa8b0c8a615b6672dbed5ec9b428f5c5104c
duhi23 <diego.21.phs@hotmail.com>
2017-07-16 21:22:48 -0500

    Updates

--- missing_data.R
+++ missing_data.R
@@ -61,7 +61,7 @@ datos <- data.table(read_tsv("TemplateCobranzaTotal.txt"))
 vars <- colnames(datos)[as.vector(which(unlist(lapply(datos, class)) == "numeric"))]
 var_num <- as.vector(which(unlist(lapply(datos, class)) == "numeric"))
 
-# Función atipico 
+# Función is.atipico 
 # data: datos en formato data.table sin valores perdidos
 # vars: vector de variables numéricas
 # retorna vector de nombres de variables con valores atipicos
@@ -76,9 +76,24 @@ is.atipico <- function(data, vars){
 
 vars_atp <- is.atipico(datos, vars)
 
-tabla <- data.frame()
+tabla <- data.frame(variable=c("SALDO_NOR_M1", "SALDO_NOR_M5", "SALDO_NOR_M10", "SALDO_NOR_M11"), 
+                    accion=c("eliminar", "reemplazar", "eliminar", "reemplazar"), 
+                    stringsAsFactors = FALSE)
 
 
-atipico <- function(data, criterio){
-      nvars <- nrow(criterio)
+# Función atipico
+# data: datos en formato data.table sin valores perdidos
+# criterio: tabla con las acciones a tomar sobre las variables
+# vars: vector de variables con valores atipicos
+# 
+atipico <- function(data, criterio, vars){
+      sub_cri <- criterio[criterio[,2]=="eliminar",]
+      nvars <- nrow(sub_cri)
+      if(nvars>0){
+            cols_dlt <- c()
+            for(i in 1:nvars){
+                  cols_dlt <- c(grep(sub_cri[[1]][i], vars)[1], cols_dlt)
+            }
+      }
 }
+
